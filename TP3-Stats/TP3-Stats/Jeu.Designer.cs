3f80d0232a169084a3937c83f7ccde6958761dab
PoneyLoverXz <antho.voyez@hotmail.com>
2015-03-24 11:45:47 -0400

    Joueur vs Joueur marche

--- TP3-Stats/TP3-Stats/Jeu.Designer.cs
+++ TP3-Stats/TP3-Stats/Jeu.Designer.cs
@@ -28,53 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.PB_J1 = new System.Windows.Forms.PictureBox();
+            this.PB_Paquet = new System.Windows.Forms.PictureBox();
             this.LB_Score1 = new System.Windows.Forms.Label();
             this.LB_Score2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.PB_J1)).BeginInit();
+            this.LB_J1 = new System.Windows.Forms.Label();
+            this.LB_J2 = new System.Windows.Forms.Label();
+            this.BTN_Passer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Paquet)).BeginInit();
             this.SuspendLayout();
             // 
-            // PB_J1
+            // PB_Paquet
             // 
-            this.PB_J1.Image = global::TP3_Stats.Properties.Resources.Dos_Carte_J1;
-            this.PB_J1.Location = new System.Drawing.Point(395, 430);
-            this.PB_J1.Name = "PB_J1";
-            this.PB_J1.Size = new System.Drawing.Size(211, 314);
-            this.PB_J1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.PB_J1.TabIndex = 0;
-            this.PB_J1.TabStop = false;
-            this.PB_J1.Click += new System.EventHandler(this.PB_J1_Click);
+            this.PB_Paquet.Image = global::TP3_Stats.Properties.Resources.Dos_Carte_J1;
+            this.PB_Paquet.Location = new System.Drawing.Point(140, 214);
+            this.PB_Paquet.Name = "PB_Paquet";
+            this.PB_Paquet.Size = new System.Drawing.Size(211, 314);
+            this.PB_Paquet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_Paquet.TabIndex = 0;
+            this.PB_Paquet.TabStop = false;
+            this.PB_Paquet.Click += new System.EventHandler(this.PB_Paquet_Click);
             // 
             // LB_Score1
             // 
             this.LB_Score1.AutoSize = true;
-            this.LB_Score1.Location = new System.Drawing.Point(341, 113);
+            this.LB_Score1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Score1.Location = new System.Drawing.Point(194, 104);
             this.LB_Score1.Name = "LB_Score1";
-            this.LB_Score1.Size = new System.Drawing.Size(13, 13);
+            this.LB_Score1.Size = new System.Drawing.Size(24, 25);
             this.LB_Score1.TabIndex = 1;
             this.LB_Score1.Text = "0";
             // 
             // LB_Score2
             // 
             this.LB_Score2.AutoSize = true;
-            this.LB_Score2.Location = new System.Drawing.Point(593, 113);
+            this.LB_Score2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Score2.Location = new System.Drawing.Point(327, 104);
             this.LB_Score2.Name = "LB_Score2";
-            this.LB_Score2.Size = new System.Drawing.Size(13, 13);
+            this.LB_Score2.Size = new System.Drawing.Size(24, 25);
             this.LB_Score2.TabIndex = 2;
             this.LB_Score2.Text = "0";
+            // 
+            // LB_J1
+            // 
+            this.LB_J1.AutoSize = true;
+            this.LB_J1.Location = new System.Drawing.Point(137, 113);
+            this.LB_J1.Name = "LB_J1";
+            this.LB_J1.Size = new System.Drawing.Size(51, 13);
+            this.LB_J1.TabIndex = 3;
+            this.LB_J1.Text = "Joueur1: ";
+            // 
+            // LB_J2
+            // 
+            this.LB_J2.AutoSize = true;
+            this.LB_J2.Location = new System.Drawing.Point(270, 113);
+            this.LB_J2.Name = "LB_J2";
+            this.LB_J2.Size = new System.Drawing.Size(51, 13);
+            this.LB_J2.TabIndex = 3;
+            this.LB_J2.Text = "Joueur2: ";
+            // 
+            // BTN_Passer
+            // 
+            this.BTN_Passer.Location = new System.Drawing.Point(211, 185);
+            this.BTN_Passer.Name = "BTN_Passer";
+            this.BTN_Passer.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Passer.TabIndex = 4;
+            this.BTN_Passer.Text = "Passer";
+            this.BTN_Passer.UseVisualStyleBackColor = true;
+            this.BTN_Passer.Click += new System.EventHandler(this.BTN_Passer_Click);
             // 
             // Jeu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LimeGreen;
-            this.ClientSize = new System.Drawing.Size(1060, 806);
+            this.ClientSize = new System.Drawing.Size(502, 530);
+            this.Controls.Add(this.BTN_Passer);
+            this.Controls.Add(this.LB_J2);
+            this.Controls.Add(this.LB_J1);
             this.Controls.Add(this.LB_Score2);
             this.Controls.Add(this.LB_Score1);
-            this.Controls.Add(this.PB_J1);
+            this.Controls.Add(this.PB_Paquet);
             this.Name = "Jeu";
             this.Text = "Jeu";
-            ((System.ComponentModel.ISupportInitialize)(this.PB_J1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Paquet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,8 +118,11 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox PB_J1;
+        private System.Windows.Forms.PictureBox PB_Paquet;
         private System.Windows.Forms.Label LB_Score1;
         private System.Windows.Forms.Label LB_Score2;
+        private System.Windows.Forms.Label LB_J1;
+        private System.Windows.Forms.Label LB_J2;
+        private System.Windows.Forms.Button BTN_Passer;
     }
 }
--- TP3-Stats/TP3-Stats/Jeu.cs
+++ TP3-Stats/TP3-Stats/Jeu.cs
@@ -16,45 +16,111 @@ namespace TP3_Stats
 
         int Tour = 1;
 
+        bool J1_Fini = false;
+        bool J2_Fini = false;
+
         public Jeu()
         {
             InitializeComponent();
+            LB_J1.BackColor = System.Drawing.Color.AntiqueWhite;
         }
 
-        private void PB_J1_Click(object sender, EventArgs e)
+        private void PB_Paquet_Click(object sender, EventArgs e)
         {
            // Carte au hasard
             Carte carte = paquet.PigerCarte();
 
-            PB_J1.Image = carte.Image_;
+            PB_Paquet.Image = carte.Image_;
 
             if(Tour == 1)
                 LB_Score1.Text = (int.Parse(LB_Score1.Text) + carte.Valeur_).ToString();
             else
                 LB_Score2.Text = (int.Parse(LB_Score2.Text) + carte.Valeur_).ToString();
 
-            ChangerTour();
             VérifierVictoire();
+            ChangerTour();
         }
 
         private void ChangerTour()
         {
             if (Tour == 1)
-                Tour = 2;
-            else
-                Tour = 1;
+            {
+                if(!J2_Fini)
+                {
+                    Tour = 2;
+                    LB_J1.BackColor = System.Drawing.Color.Transparent;
+                    LB_J2.BackColor = System.Drawing.Color.AntiqueWhite;
+                }
+            }
+            else if(Tour == 2 && !J1_Fini)
+            {
+                if(!J1_Fini)
+                {
+                    Tour = 1;
+                    LB_J1.BackColor = System.Drawing.Color.AntiqueWhite;
+                    LB_J2.BackColor = System.Drawing.Color.Transparent;
+                }
+            }
         }
         
         private void VérifierVictoire()
         {
+            if (int.Parse(LB_Score1.Text) == 21 && int.Parse(LB_Score2.Text) == 21)
+            {
+                MessageBox.Show("Match nul!");
+                this.Close();
+            }
+            else
+            {
+                if (int.Parse(LB_Score1.Text) == 21)
+                {
+                    if(Tour == 2)
+                    {
+                       MessageBox.Show("Victoire du joueur 1!");
+                       this.Close();
+                    }
+                }
+                if (int.Parse(LB_Score2.Text) == 21)
+                {
+                       MessageBox.Show("Victoire du joueur 2!");
+                       this.Close();
+                }
+            }
+            
             if (int.Parse(LB_Score1.Text) > 21)
+            {
                 MessageBox.Show("Victoire du joueur 2!");
-            else if (int.Parse(LB_Score1.Text) == 21)
-                MessageBox.Show("Victoire du joueur 1!");
+                this.Close();
+            }
             else if (int.Parse(LB_Score2.Text) > 21)
+            {
                 MessageBox.Show("Victoire du joueur 1!");
-            else if (int.Parse(LB_Score2.Text) == 21)
-                MessageBox.Show("Victoire du joueur 2!");
+                this.Close();
+            }
+            else if (J1_Fini && J2_Fini)
+            {
+                if(int.Parse(LB_Score1.Text) > int.Parse(LB_Score2.Text))
+                {
+                    MessageBox.Show("Victoire du joueur 1!");
+                    this.Close();
+                }
+                else if (int.Parse(LB_Score1.Text) < int.Parse(LB_Score2.Text))
+                {
+                    MessageBox.Show("Victoire du joueur 2!");
+                    this.Close();
+                }
+            }
+        }
+
+        private void BTN_Passer_Click(object sender, EventArgs e)
+        {
+            if (Tour == 1)
+                J1_Fini = true;
+            else
+                J2_Fini = true;
+            if (J1_Fini && J2_Fini)
+                VérifierVictoire();
+            ChangerTour();
         }
     }
 }
